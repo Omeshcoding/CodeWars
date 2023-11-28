851219221b3fb7ea20c53a52a7fff91c34319859
umesh <umesharma.us11@gmail.com>
2023-11-28 23:05:42 +0530

    Completed the alphabatically ordered problem

---
+++ Strings/alphabaticallyOrdered.js
@@ -0,0 +1,14 @@
+// Your task is very simple. Just write a function that takes an input string of lowercase letters and returns true/false depending on whether the string is in alphabetical order or not.
+// Examples (input -> output)
+
+//     "kata" -> false ('a' comes after 'k')
+//     "ant" -> true (all characters are in alphabetical order)
+
+function alphabaticallyOrdered(str) {
+  const sortedStr = Array.from(str).sort().join('');
+  return str === sortedStr;
+}
+
+alphabaticallyOrdered('abc');
+alphabaticallyOrdered('bac');
+alphabaticallyOrdered('bac');
